23a0ff7329f107d17a02a834692ea9387d3991f9
Md Shamim <shamimice03@gmail.com>
2023-01-12 23:27:31 +0900

    main file changed

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "github_repository" "github_repo" {
-  name        = var.repo_name
-  description = var.repo_description
+  name        = var.repo_name == null ? "test-repo" : var.repo_name
+  description = var.repo_description == null ? "test-repo" : var.repo_description
   visibility  = var.is_public ? "public" : "private"
 }
 
